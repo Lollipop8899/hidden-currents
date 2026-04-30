f6969a0a6aaf9facd9e3340d5fe7c9fdc99a4336
Lollipop8899 <2828dmd@gmail.com>
2026-04-30 00:10:07 -0700

    Update app.py

--- app.py
+++ app.py
@@ -519,7 +519,7 @@ if page == "Home":
 
     col1, col2 = st.columns(2)
 
-with col1:
+    with col1:
     st.markdown(f"""
     <div style="
         padding:22px 24px;
@@ -544,7 +544,7 @@ with col1:
     </div>
     """, unsafe_allow_html=True)
 
-with col2:
+    with col2:
     st.markdown(f"""
     <div style="
         padding:22px 24px;
